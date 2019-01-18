0426753b7adf83b3b52d8a65cf2a68f1b7d5a450
RupaliTaware <46775699+RupaliTaware@users.noreply.github.com>
2019-01-18 13:20:35 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,8 @@
+def swap(a,b):
+    print("The values of are swap as")
+    print("X={}" .format(b))
+    print("Y={}" .format(a))
+    
+x=input("Enter the value for X ")
+y=input("Enter the value for Y ")
+swap(x,y)
