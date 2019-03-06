57c6348f619acb5abe02b694abcb0fae181610e8
RupaliTaware <46775699+RupaliTaware@users.noreply.github.com>
2019-03-06 11:48:53 +0530

    Update divisorof.py

--- divisorof.py
+++ divisorof.py
@@ -11,36 +11,9 @@ def divisor(num):
 nd=int(input('Enter no to find its Divisors'))
 divisor(nd)
 
-def palidrome(my_str):
-    l=(len(my_str)-1)
-    i=0
-    while(i<=l):
-        if(my_str[i]==my_str[l-i]):
-            i+=1
-        else:
-            print('Given string {} is Not Palidrome' .format(my_str))
-            break
-        print('Given string {} is  Palidrome' .format(my_str))
-        break
-
-
-us=input('Enter any String')
-palidrome(us)
 
 
 
 
 
-def fibco(n):
 
-     sum=1
-     sum_list = [0]
-     for i in range(0, n):
-        sum_list.append(sum)
-        sum=sum+sum_list[i]
-
-     print('The Fabonacci of range{} is' .format(n))
-     print(sum_list)
-
-f_no=int(input('enter no of Fibonacci you want'))
-fibco(f_no)
